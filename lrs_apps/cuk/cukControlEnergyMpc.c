56d152348e9eb117ea724691fd308faa7f1dbf76
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2025-08-21 17:06:04 +0200

    Slightly faster freq domain mpc implementation

--- lrs_apps/cuk/cukControlEnergyMpc.c
+++ lrs_apps/cuk/cukControlEnergyMpc.c
@@ -48,6 +48,19 @@ typedef struct{
 /*--------------------------------- Globals ---------------------------------*/
 //=============================================================================
 
+#define Ky      (4.015134334564209f)
+#define K_dz_1  (114.55923461914062f)
+#define K_dz_2  (0.016813231632113457f)
+
+#define DT      ((float)(1.0f/100e3))
+
+#define ALPHA   ((float)1e6)
+
+#define CO      ((float)330e-6)
+
+#define IL_MAX  6.0f
+#define IL_MIN  0.15f
+
 static float p_in = 0.0f, p_out = 0.0f;
 
 /* y_1 and y_dot_1 are past samples of y and y_dot */
@@ -66,11 +79,12 @@ static float D[DMPC_CONFIG_L_PAST];
 
 static float aux1 = 0.0f, aux2 = 0.0f;
 
-static params_t params = {
-    .Ky = 4.142886638641357f, .K_dz_1 = 108.40249633789062f, .K_dz_2 = 0.01472429372370243f,
-    .dt=1.0f/100e3, .alpha = 1e6, .Co = CUK_CONFIG_C_O,
-    .il_max = 6.0f, .il_min = 0.15f
-};
+static params_t params;
+// static params_t params = {
+//     .Ky = 4.142886638641357f, .K_dz_1 = 108.40249633789062f, .K_dz_2 = 0.01472429372370243f,
+//     .dt=1.0f/100e3, .alpha = 1e6, .Co = CUK_CONFIG_C_O,
+//     .il_max = 6.0f, .il_min = 0.15f
+// };
 //=============================================================================
 
 //=============================================================================
@@ -122,7 +136,7 @@ int32_t cukControlEnergyMpcRun(void *meas, int32_t nmeas, void *refs, int32_t nr
     e_x1 = (0.5f) * CUK_CONFIG_L_IN * x1 * x1;
     e_x2 = (0.5f) * CUK_CONFIG_L_OUT * x2 * x2;
     e_x3 = (0.5f) * CUK_CONFIG_C_C * x3 * x3 * CUK_CONFIG_TF_N2N1_SQ / (CUK_CONFIG_TF_N2N1_SQ + 1.0f);
-    e_x4 = (0.5f) * params.Co * x4 * x4;
+    e_x4 = (0.5f) * CO * x4 * x4;
     y = e_x1 + e_x2 + e_x3 + e_x4;
 
     /* Input, output and converter power */
@@ -139,7 +153,7 @@ int32_t cukControlEnergyMpcRun(void *meas, int32_t nmeas, void *refs, int32_t nr
     e_x1_r = (0.5f) * CUK_CONFIG_L_IN * x1_r * x1_r;
     e_x2_r = (0.5f) * CUK_CONFIG_L_OUT * x2_r * x2_r;
     e_x3_r = (0.5f) * CUK_CONFIG_C_C * x3_r * x3_r * CUK_CONFIG_TF_N2N1_SQ / (CUK_CONFIG_TF_N2N1_SQ + 1.0f);
-    e_x4_r = (0.5f) * params.Co * x4_r * x4_r;
+    e_x4_r = (0.5f) * CO * x4_r * x4_r;
     yr = e_x1_r + e_x2_r + e_x3_r + e_x4_r;
 
     /* Initialization */
@@ -150,21 +164,21 @@ int32_t cukControlEnergyMpcRun(void *meas, int32_t nmeas, void *refs, int32_t nr
     }
 
     /* Delay compensation */
-    y = y + params.dt * y_dot + params.alpha * params.dt * params.dt / 2.0f * v_1;
-    y_dot = y_dot + params.alpha * params.dt * v_1;
+    y = y + DT * y_dot + ALPHA * DT * DT / 2.0f * v_1;
+    y_dot = y_dot + ALPHA * DT * v_1;
 
     /* Determine bounds for dv */
-    dv_min_v = hwm->vi_dc / CUK_CONFIG_L_IN * (hwm->vi_dc - x3) / params.alpha - v_1;
-    dv_max_v = hwm->vi_dc * hwm->vi_dc / CUK_CONFIG_L_IN / params.alpha - v_1;
+    dv_min_v = hwm->vi_dc / CUK_CONFIG_L_IN * (hwm->vi_dc - x3) / ALPHA - v_1;
+    dv_max_v = hwm->vi_dc * hwm->vi_dc / CUK_CONFIG_L_IN / ALPHA - v_1;
 
-    dv_min_z2 = (params.il_min * hwm->vi_dc - hwm->vo * hwm->io - 2.0f * y_dot + y_dot_1) / (params.alpha * params.dt);
-    dv_max_z2 = (params.il_max * hwm->vi_dc - hwm->vo * hwm->io - 2.0f * y_dot + y_dot_1) / (params.alpha * params.dt);
+    dv_min_z2 = (IL_MIN * hwm->vi_dc - hwm->vo * hwm->io - 2.0f * y_dot + y_dot_1) / (ALPHA * DT);
+    dv_max_z2 = (IL_MAX * hwm->vi_dc - hwm->vo * hwm->io - 2.0f * y_dot + y_dot_1) / (ALPHA * DT);
 
     dv_min = dv_min_v > dv_min_z2 ? dv_min_v : dv_min_z2;
     dv_max = dv_max_v < dv_max_z2 ? dv_max_v : dv_max_z2;
 
     /* Optimization */
-    dv = - params.Ky * (y - yr) - params.K_dz_1 * (y - y_1) - params.K_dz_2 * (y_dot - y_dot_1);
+    dv = - Ky * (y - yr) - K_dz_1 * (y - y_1) - K_dz_2 * (y_dot - y_dot_1);
     //mulmv(DMPC_Ku_freq, 1, D, DMPC_CONFIG_L_PAST, &aux2);
     for(i = 0; i < DMPC_CONFIG_L_PAST; i++) aux2 += DMPC_Ku_freq[i] * D[i];
     dv = dv - aux1 * v_1 - aux2;
@@ -179,7 +193,7 @@ int32_t cukControlEnergyMpcRun(void *meas, int32_t nmeas, void *refs, int32_t nr
     aux2 = 0;
 
     /* Feedback linearization */
-    duty = 1.0f - 1.0f / x3 * (hwm->vi_dc - CUK_CONFIG_L_IN / hwm->vi_dc * params.alpha * v);
+    duty = 1.0f - 1.0f / x3 * (hwm->vi_dc - CUK_CONFIG_L_IN / hwm->vi_dc * ALPHA * v);
 
     o->u = duty;
 
@@ -242,7 +256,7 @@ int32_t cukControlEnergyMpcFirstEntry(void *meas, int32_t nmeas,
     // e_x1 = (0.5f) * CUK_CONFIG_L_IN * x1 * x1;
     // e_x2 = (0.5f) * CUK_CONFIG_L_OUT * x2 * x2;
     // e_x3 = (0.5f) * CUK_CONFIG_C_C * x3 * x3 * CUK_CONFIG_TF_N2N1_SQ / (CUK_CONFIG_TF_N2N1_SQ + 1.0f);
-    // e_x4 = (0.5f) * params.Co * x4 * x4;
+    // e_x4 = (0.5f) * CO * x4 * x4;
     // y = e_x1 + e_x2 + e_x3 + e_x4;
     //
     // /* Input, output and converter power */
@@ -250,7 +264,7 @@ int32_t cukControlEnergyMpcFirstEntry(void *meas, int32_t nmeas,
     // p_out = swm->po;
     // y_dot = p_in - p_out;
     //
-    // //v_init = hwm->vi_dc / CUK_CONFIG_L_IN * (hwm->vi_dc - (1.0f - o->u) * x3) / params.alpha;
+    // //v_init = hwm->vi_dc / CUK_CONFIG_L_IN * (hwm->vi_dc - (1.0f - o->u) * x3) / ALPHA;
     //
     // v_1 = 0;
     // y_1 = y;
