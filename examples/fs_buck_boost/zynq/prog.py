604d2fc3bb50e262a914ce15aa98129bd5ac7259
Mousavi Erfan <m_erfan@lrs.eit.uni-kl.de>
2025-10-16 17:53:01 +0200

    solving the issue with prog.py, it can be run from vitis env

--- examples/fs_buck_boost/zynq/prog.py
+++ examples/fs_buck_boost/zynq/prog.py
@@ -1,5 +1,6 @@
 import xsdb
 import time
+import subprocess
 
 # --- Input ---
 ws_path = './build'
@@ -18,6 +19,12 @@ fpga_bs = f'{ws_path}/{platform_name}/hw/pynq_lrssoc.bit'
 fsbl = f'{ws_path}/{platform_name}/zynq_fsbl/build/fsbl.elf'
 cpu0_elf = f'{ws_path}/{cpu0_app_name}/build/{cpu0_app_name}.elf'
 cpu1_elf = f'{ws_path}/{cpu1_app_name}/build/{cpu1_app_name}.elf'
+
+# --- Launch hw_server via xsdb CLI ---
+xsdb_path = r"C:\Xilinx\Vitis\2024.2\bin\xsdb.bat"  # <- Update if needed
+subprocess.run([xsdb_path, "-eval", "connect; exit"], check=True)
+time.sleep(3)  
+
 
 # --- Program ---
 s = xsdb.start_debug_session()
